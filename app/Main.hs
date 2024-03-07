0d8c926b3572f0c8510e045f1d0f5da7a4b45a87
Thomaltarix <thomas.boue@epitech.eu>
2024-03-07 18:21:01 +0100

    :construction: Add Work In Progress

--- app/Main.hs
+++ app/Main.hs
@@ -7,5 +7,15 @@
 
 module Main (main) where
 
+import Parsing (parseArgs)
+import FileParsing (fileParsing)
+import System.Environment
+import System.Exit
+import System.IO (hPutStrLn, stderr)
+
 main :: IO ()
-main = return ()
+main = do
+    args <- getArgs
+    case parseArgs args of
+        Nothing -> hPutStrLn stderr "Invalid input" >> exitWith(ExitFailure 84)
+        Just opt -> fileParsing opt
---
+++ src/FileParsing.hs
@@ -0,0 +1,13 @@
+{-
+-- EPITECH PROJECT, 2024
+-- ImageCompressor
+-- File description:
+-- FileParsing
+-}
+
+module FileParsing (fileParsing) where
+
+import Parsing (Options(..))
+
+fileParsing :: Options -> IO ()
+fileParsing opt = return ()

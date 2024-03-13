be8450308b61859ca3bcbaf522089a72d7c66b38
Thomaltarix <thomas.boue@epitech.eu>
2024-03-13 18:22:00 +0100

    :sparkles: Update the main with the added features

--- app/Main.hs
+++ app/Main.hs
@@ -15,9 +15,17 @@ import System.IO (hPutStrLn, stderr)
 exit :: String -> IO ()
 exit str = hPutStrLn stderr str >> exitWith (ExitFailure 84)
 
+getOptionsFilePath :: Maybe Options -> String
+getOptionsFilePath (Just opt) = case filePath opt of
+    Just path -> path
+    Nothing -> ""
+getOptionsFilePath _ = ""
+
 main :: IO ()
 main = do
     args <- getArgs
-    case parseArgs args of
-        Nothing -> hPutStrLn stderr "Invalid input" >> exitWith(ExitFailure 84)
-        Just opt -> fileParsing opt
+    let opt = parseArgs args
+    content <- getFile (Just (getOptionsFilePath opt))
+    case content of
+        Nothing -> exit "Error: file not found"
+        Just content' -> imageCompressor (fillConf content') opt

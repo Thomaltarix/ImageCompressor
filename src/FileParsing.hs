55066fad5de1d5746c54fae3eae9ae7b4f6a5b36
Thomaltarix <thomas.boue@epitech.eu>
2024-03-13 18:18:26 +0100

    :sparkles: Add a function to safely get the given file content

--- src/FileParsing.hs
+++ src/FileParsing.hs
@@ -8,6 +8,15 @@
 module FileParsing (fileParsing) where
 
 import Parsing (Options(..))
+getFile :: Maybe String -> IO (Maybe String)
+getFile (Just path) = do
+    file <- try $ openFile path ReadMode :: IO (Either SomeException Handle)
+    case file of
+        Left _ -> return Nothing
+        Right file -> do
+            content <- hGetContents file
+            return $ Just content
+getFile _ = return Nothing
 
 fileParsing :: Options -> IO ()
 fileParsing opt = return ()

e3e88bd2f46e50bfd529608e9f863e4cc8b91a1f
Thomaltarix <thomas.boue@epitech.eu>
2024-03-14 17:21:25 +0100

    :sparkles: Add the file parsing

--- src/FileParsing.hs
+++ src/FileParsing.hs
@@ -11,6 +11,7 @@ module FileParsing (getFile,
 
 import System.IO
 import Control.Exception
+import Data.Char
 
 data Vector = Vector [Double] deriving (Show)
 
@@ -28,5 +29,35 @@ getFile (Just path) = do
             return $ Just content
 getFile _ = return Nothing
 
+addToVector :: Vector -> Double -> Type -> Vector
+addToVector (Vector vec) value valType
+    | valType == Coord && isCoord value = Vector (value : vec)
+    | valType == Color && isColor value = Vector (value : vec)
+    | otherwise = Vector []
+
+parseLine :: String -> String -> Type -> Vector
+parseLine "" _ _ = Vector []
+parseLine ('(':xs) "" valType = parseLine xs "" valType
+parseLine ('(':xs) str valType =
+    addToVector (parseLine xs "" valType) (read str :: Double) valType
+parseLine (')':xs) "" valType = parseLine xs "" valType
+parseLine (')':xs) str valType =
+    addToVector (parseLine xs "" valType) (read str :: Double) valType
+parseLine (',':xs) "" valType = parseLine xs "" valType
+parseLine (',':xs) str valType =
+    addToVector (parseLine xs "" valType) (read str :: Double) valType
+parseLine (x:xs) str valType
+    | isDigit x = parseLine xs (str ++ [x]) valType
+    | otherwise = Vector []
+
+parseFile :: [String] -> [(Vector, Vector)]
+parseFile [] = []
+parseFile (x1:x2:xs) = (parseLine x1 "" Coord, parseLine x2 "" Color) : parseFile xs
+parseFile _ = []
 fillConf :: String -> Maybe Conf
-fillConf _ = Nothing
+fillConf str = do
+    let lines' = words str
+    let vectorList = parseFile lines'
+    if length vectorList == getNbLines str && checkVectorList vectorList
+        then Just (Conf vectorList)
+        else Nothing

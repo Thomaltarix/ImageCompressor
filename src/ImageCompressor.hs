d80489994e02912eeba025bd843e5da8438076d4
Thomaltarix <thomas.boue@epitech.eu>
2024-03-14 17:19:51 +0100

    :bulb: Refactor comments to print useful things

--- src/ImageCompressor.hs
+++ src/ImageCompressor.hs
@@ -11,5 +11,6 @@ import FileParsing (Conf(..))
 import Parsing (Options(..))
 
 imageCompressor :: Maybe Conf -> Maybe Options -> IO ()
-imageCompressor (Just _) (Just _) = putStrLn "imageCompressor no error"
-imageCompressor _ _ = putStrLn "imageCompressor error"
+imageCompressor (Just _) (Just _) = putStrLn "Success"
+imageCompressor Nothing _ = putStrLn "Wrong file content"
+imageCompressor _ _ = putStrLn "Wrong Options"
